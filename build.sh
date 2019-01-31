1813656540cca5af71d49231b34e739a0f18fc36
Wei Dong <wei.dong@iailabs.com>
2019-01-30 21:58:36 -0500

    add

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 export CONFIG_DIR=`readlink -e $PWD`
 
-pushd donkey_src/src
+pushd donkey/src
 ./setup.py build
 popd
-cp donkey_src/src/build/lib.linux-x86_64-*/donkey.cpython-*.so ./
+cp donkey/src/build/lib.linux-x86_64-*/donkey.cpython-*.so ./
 

c23e1a21a59dc50230b377f187c1650a1e991994
Wei Dong <wdong@wdong.org>
2019-01-31 12:23:32 -0500

    non interactive

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -it --rm -p 18877:8888 \
+docker run --rm -p 18877:8888 \
     -v /u07/wei/face_search_engine/faces:/face-search-engine/data \
     -v /u07/wei/face_search_engine/db:/face-search-engine/db \
     aaalgo/face_search_engine $*
